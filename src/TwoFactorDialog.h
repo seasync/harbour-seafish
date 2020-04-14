a898844934c94943c6ae5af7006383f14755740e
bionade24 <o.roesler@oscloud.info>
2020-04-14 15:22:05 +0200

    Add class methods and properties of src/TwoFactorDialog.h

--- src/TwoFactorDialog.h
+++ src/TwoFactorDialog.h
@@ -8,12 +8,18 @@ class TwoFactorDialog : public QObject
     Q_OBJECT
 public:
     explicit TwoFactorDialog(QObject *parent = nullptr);
+    bool finished();
+    QString getText();
+    bool rememberDeviceChecked();
 
 signals:
     void Token_needed();
 
 public slots:
     void twoFAToken(QString token);
+
+private:
+    QString token_;
 };
 
 #endif // TWOFACTORDIALOG_H
