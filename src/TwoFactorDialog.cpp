5e010675132a6a91ff70e723e00e3b96313c1635
bionade24 <o.roesler@oscloud.info>
2020-04-14 15:24:14 +0200

    Implement TwoFactorDialog::getText()

--- src/TwoFactorDialog.cpp
+++ src/TwoFactorDialog.cpp
@@ -5,3 +5,7 @@ TwoFactorDialog::TwoFactorDialog(QObject *parent) : QObject(parent) {}
 bool TwoFactorDialog::finished() {
     return !token_.isEmpty();
 }
+
+QString TwoFactorDialog::getText() {
+    return token_;
+}
